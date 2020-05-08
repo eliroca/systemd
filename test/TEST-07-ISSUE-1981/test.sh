4daeed5256afa4a27ee93f239f691750daeeb90b
Elisei Roca <eroca@suse.de>
2020-05-08 19:33:28 +0200

    TEST-07-ISSUE-1981: adapt for openQA

--- test/TEST-07-ISSUE-1981/test.sh
+++ test/TEST-07-ISSUE-1981/test.sh
@@ -3,6 +3,7 @@ set -e
 TEST_DESCRIPTION="https://github.com/systemd/systemd/issues/1981"
 TEST_NO_QEMU=1
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
 NSPAWN_TIMEOUT=30
@@ -29,10 +30,38 @@ Type=oneshot
 EOF
 
         cp test-segfault.sh $initdir/
-
+        cp test-segfault.sh /
+        cp $initdir/etc/systemd/system/testsuite.service /etc/systemd/system/testsuite.service
+        mkdir -p /lib/systemd/system
         setup_testsuite
     )
     setup_nspawn_root
+}
+
+test_run() {
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    if [ -z "$TEST_NO_NSPAWN" ]; then
+        if run_nspawn "nspawn-root"; then
+            check_result_nspawn "nspawn-root" || return 1
+        else
+            dwarn "can't run systemd-nspawn, skipping"
+        fi
+    fi
+    ret=1
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
+test_cleanup() {
+    _test_cleanup
+    rm -f /etc/systemd/system/testsuite.service
+    rm -rf /lib/systemd/system
+    for file in $(ls /testok* /failed* /test-segfault.sh 2>/dev/null); do
+      rm $file
+    done
+    return 0
 }
 
 do_test "$@"
