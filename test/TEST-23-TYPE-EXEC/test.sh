3661c09d2e20d1b85e981671d8986fd94f302ff7
Elisei Roca <eroca@suse.de>
2020-05-08 18:24:40 +0200

    TEST-23-TYPE-EXEC: adapt for openQA

--- test/TEST-23-TYPE-EXEC/test.sh
+++ test/TEST-23-TYPE-EXEC/test.sh
@@ -2,6 +2,7 @@
 set -e
 TEST_DESCRIPTION="test Type=exec"
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
 test_setup() {
@@ -24,10 +25,38 @@ ExecStart=/testsuite.sh
 Type=oneshot
 EOF
         cp testsuite.sh $initdir/
+        cp testsuite.sh /
+        cp $initdir/etc/systemd/system/testsuite.service /etc/systemd/system/
 
         setup_testsuite
     )
     setup_nspawn_root
+}
+
+test_run() {
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    if [ -z "$TEST_NO_NSPAWN" ]; then
+        if run_nspawn "nspawn-root"; then
+            check_result_nspawn "nspawn-root" || return 1
+        else
+            dwarn "can't run systemd-nspawn, skipping"
+        fi
+    fi
+    ret=1
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
+test_cleanup() {
+    _test_cleanup
+    rm -f /testsuite.sh
+    rm -f /etc/systemd/system/testsuite.service
+    for file in $(ls /testok* /failed* 2>/dev/null); do
+      rm $file
+    done
+    return 0
 }
 
 do_test "$@"
