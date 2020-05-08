07a1df8d6ba243ac3494525782c95f7fa054d3fe
Elisei Roca <eroca@suse.de>
2020-05-08 15:36:32 +0200

    TEST-42-EXECSTOPPOST: adapt for openQA

--- test/TEST-42-EXECSTOPPOST/test.sh
+++ test/TEST-42-EXECSTOPPOST/test.sh
@@ -2,6 +2,7 @@
 set -e
 TEST_DESCRIPTION="test that ExecStopPost= is always run"
 
+export TEST_BASE_DIR=/var/opt/systemd-tests/test
 . $TEST_BASE_DIR/test-functions
 
 test_setup() {
@@ -40,10 +41,41 @@ ExecStart=/testsuite.sh
 Type=oneshot
 EOF
         cp testsuite.sh $initdir/
+        cp testsuite.sh /
+        cp $initdir/etc/systemd/system/testsuite.service /etc/systemd/system/
+        mkdir -p /etc/dbus-1/system.d
+        cp $initdir/etc/dbus-1/system.d/systemd.test.ExecStopPost.conf /etc/dbus-1/system.d/
 
         setup_testsuite
     )
     setup_nspawn_root
+}
+
+test_run() {
+    systemctl daemon-reload
+    systemctl start testsuite.service || return 1
+    if [ -z "$TEST_NO_NSPAWN" ]; then
+        if run_nspawn "nspawn-root"; then
+            check_result_nspawn "nspawn-root" || return 1
+        else
+            dwarn "can't run systemd-nspawn, skipping"
+        fi
+    fi
+    ret=1
+    test -s /failed && ret=$(($ret+1))
+    [[ -e /testok ]] && ret=0
+    return $ret
+}
+
+test_cleanup() {
+    _test_cleanup
+    rm -f /etc/dbus-1/system.d/systemd.test.ExecStopPost.conf
+    rm -f /etc/systemd/system/testsuite.service
+    rm -f /testsuite.sh
+    for file in $(ls /testok* /failed* 2>/dev/null); do
+      rm $file
+    done
+    return 0
 }
 
 do_test "$@"
